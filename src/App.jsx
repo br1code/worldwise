d9d69bb0409255d09de116770490772bab58bf9c
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2023-11-22 16:25:40 -0300

    adding react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Homepage />,
+    },
+    {
+        path: "product",
+        element: <Product />,
+    },
+    {
+        path: "pricing",
+        element: <Pricing />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
+
 export default function App() {
-    return <div>Worldwise</div>;
+    return <RouterProvider router={router} />;
 }
