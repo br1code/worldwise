86a27e60848728e543042d0f48b00a16db45136d
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2023-11-23 13:17:29 -0300

    a bunch of work

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,66 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import Pricing from "./pages/Pricing.jsx";
-import NotFound from "./pages/NotFound.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
+import CityList from "./components/CityList.jsx";
+import { useEffect, useState } from "react";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Homepage />,
-    },
-    {
-        path: "product",
-        element: <Product />,
-    },
-    {
-        path: "pricing",
-        element: <Pricing />,
-    },
-    {
-        path: "*",
-        element: <NotFound />,
-    },
-]);
+const API_URL = "http://localhost:9000";
 
 export default function App() {
-    return <RouterProvider router={router} />;
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${API_URL}/cities`);
+
+                if (!res.ok) {
+                    throw new Error("Failed to fetch cities");
+                }
+
+                const data = await res.json();
+                setCities(data);
+            } catch (error) {
+                console.error(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchCities();
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="login" element={<Login />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route
+                        index
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
+                    <Route
+                        path="cities"
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
+                    <Route path="countries" element={<p>Countries</p>} />
+                    <Route path="form" element={<p>Form</p>} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
